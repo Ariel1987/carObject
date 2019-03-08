95edfa7b6134592d8049f02171e0e88be0abf732
Ariel1987 <38384419+Ariel1987@users.noreply.github.com>
2019-03-08 14:14:14 -0300

    Add files via upload

--- carObject.java
+++ carObject.java
@@ -6,55 +6,71 @@ public class carObject {
 	public int ano;
 	public String modelo;
 	
+	public void imprimir() {
+        System.out.println("Quantidade de portas: " + portas);
+        System.out.println("Cor: " + cor);
+        System.out.println("Ano: " + ano);
+        System.out.println("Modelo: " + modelo);
+         
+  }
+   
+  public int getPortas() {
+        return portas;
+  }
+  public void setPortas(int portas) {
+        this.portas = portas;
+  }
+  public String getCor() {
+        return cor;
+  }
+  public void setCor(String cor) {
+        this.cor = cor;
+  }
+  public int getAno() {
+        return ano;
+  }
+  public void setAno(int ano) {
+        this.ano = ano;
+  }
+  public String getModelo() {
+        return modelo;
+  }
+  public void Modelo(String modelo) {
+        this.modelo = modelo;
+  }
+   
+
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		carObject ford = new carObject();
-		ford.portas = 4;
-		ford.cor = "azul";
-		ford.ano = 1987;
-		ford.modelo = "Ford";
-		
-		carObject fiat = new carObject();
-		fiat.portas = 2;
-		fiat.cor = "preto";
-		fiat.ano = 1999;
-		fiat.modelo = "Fiat";
-		
-		carObject gm = new carObject();
-		gm.portas = 3;
-		gm.cor = "branco";
-		gm.ano = 2015;
-		gm.modelo = "GM";
-		
-		carObject wv = new carObject();
-		wv.portas = 4;
-		wv.cor = "branco";
-		wv.ano = 2018;
-		wv.modelo = "WV";
-		
-		System.out.println(ford.portas);
-		System.out.println(ford.cor);
-		System.out.println(ford.ano);
-		System.out.println(ford.modelo);
-		
-		System.out.println(fiat.portas);
-		System.out.println(fiat.cor);
-		System.out.println(fiat.ano);
-		System.out.println(fiat.modelo);
-
-		System.out.println(gm.portas);
-		System.out.println(gm.cor);
-		System.out.println(gm.ano);
-		System.out.println(gm.modelo);
-
-		System.out.println(wv.portas);
-		System.out.println(wv.cor);
-		System.out.println(wv.ano);
-		System.out.println(wv.modelo);
-		 
+		    carObject ford = new carObject();
+		    ford.setPortas(4);
+		    ford.setCor("azul");
+		    ford.setAno(1987);
+		    ford.Modelo("Ford");
+		    ford.imprimir();
+		    
+		    carObject fiat = new carObject();
+		    fiat.setPortas(3);
+		    fiat.setCor("branco");
+		    fiat.setAno(2015);
+		    fiat.Modelo("Fiat");
+		    fiat.imprimir();
+		    
+		    carObject gm = new carObject();
+		    gm.setPortas(4);
+		    gm.setCor("azul");
+		    gm.setAno(1987);
+		    gm.Modelo("General Motors");
+		    gm.imprimir();
+		    
+		    carObject vw = new carObject();
+		    vw.setPortas(4);
+		    vw.setCor("azul");
+		    vw.setAno(1987);
+		    vw.Modelo("Volkswagen");
+		    vw.imprimir();
+		    
+		    
 	}
-	
 }
-
-
